7fb381452e5a4dbde90286c796c56fd7548eae01
stacy-s <stacey.stepanova@gmail.com>
2020-01-19 13:23:35 +0400

    The classes for import data from csv-file were implemented. Function get_data in DataImportSpace return (x, y). Function get_data in DataImportSpaceDateTime return (x, y, datetime), where x, y are lists of int, datetime is list of native Python datetime objects.

--- DataImport.py
+++ DataImport.py
@@ -13,28 +13,35 @@ class DataImport:
 class DataImportMixin(object):
     chosen_columns = None
 
-    def read_data(self):
-        # self.__dataframe = pd.read_csv(self.__filename, sep=self.__sep)
-        # self.__dataframe = self.__dataframe[chosen_columns]
-        # self.__dataframe = self.__dataframe.dropna()
-        pass
+    def _read_data(self):
+        self._dataframe = pd.read_csv(self._filename, sep=self._sep)
+        self._dataframe = self._dataframe[self.chosen_columns]
+        self._dataframe = self._dataframe.dropna()
 
 
 class DataImportSpace(DataImportMixin, DataImport):
-    def __init__(self, filename, sep=',', name_col_latitude='latitude', name_col_longitude='longitude'):
+    def __init__(self, filename, sep=',', name_col_x='latitude', name_col_y='longitude'):
         super().__init__(filename=filename, sep=sep)
-        self._name_col_latitude = name_col_latitude
-        self._name_col_longitude = name_col_longitude
-        self.chosen_columns = [self._name_col_latitude, self._name_col_longitude]
+        self._name_col_x = name_col_x
+        self._name_col_y = name_col_y
+        self.chosen_columns = [self._name_col_x, self._name_col_y]
+
+    def get_data(self):
+        self._read_data()
+        return self._dataframe[self._name_col_x].to_list(), self._dataframe[self._name_col_y].to_list()
 
 
 class DataImportSpaceDateTime(DataImportSpace):
-    def __init__(self, filename, sep=',', name_col_latitude='latitude', name_col_longitude='longitude',
+    def __init__(self, filename, sep=',', name_col_x='latitude', name_col_y='longitude',
                  name_col_date='datetaken'):
-        super().__init__(filename=filename, sep=sep, name_col_latitude=name_col_latitude,
-                         name_col_longitude=name_col_longitude)
+        super().__init__(filename=filename, sep=sep, name_col_x=name_col_x,
+                         name_col_y=name_col_y)
         self._name_col_date = name_col_date
-        self.chosen_columns = [self._name_col_latitude, self._name_col_longitude, self._name_col_date]
+        self.chosen_columns = [self._name_col_x, self._name_col_y, self._name_col_date]
+
+    def get_data(self):
+        x, y = super().get_data()
+        return x, y, pd.to_datetime(self._dataframe[self._name_col_date]).to_list()
 
 
 
