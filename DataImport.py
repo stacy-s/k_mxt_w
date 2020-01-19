a5ac5a095601998a9e9cb742056a4fc528bd5b49
stacy-s <stacey.stepanova@gmail.com>
2020-01-19 15:29:07 +0400

    Revert "ClustersData was created for rationing data array. ClustersDataSpace was cteated to store data space. SpaceMetricsMixin has function to calculate euclidean distance. ClustersDataSpaceEuclidean  stores data space and calculates euclidean distance. "
    This reverts commit a166ce1176b98aeb0b1055fcb35467f14fde3060.

--- DataImport.py
+++ DataImport.py
@@ -1,6 +1,5 @@
 from abc import abstractmethod
 import pandas as pd
-import datetime
 
 
 class DataImport:
@@ -42,7 +41,7 @@ class DataImportSpaceDateTime(DataImportSpace):
 
     def get_data(self):
         x, y = super().get_data()
-        return x, y, [dt.to_pydatetime() for dt in pd.to_datetime(self._dataframe[self._name_col_date])]
+        return x, y, pd.to_datetime(self._dataframe[self._name_col_date]).to_list()
 
 
 
