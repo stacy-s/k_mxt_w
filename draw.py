a3f1edf254619fa716e0e27bfbca15a080f5ff83
stacy-s <stacey.stepanova@gmail.com>
2020-01-22 20:50:20 +0400

    DrawingClusters shows clusters on the graph

--- draw.py
+++ draw.py
@@ -1,7 +1,7 @@
 import clustersData
 import collections
 import random
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 def rand_color():
@@ -9,29 +9,22 @@ def rand_color():
     return '0' * (2 - len(cur_color)) + cur_color
 
 
-class drawing_clusters:
-    cluster_colors = dict()
-    cluster_sizes = None
-    noise_color = '#FF0000'
-    max_noise_size = 1
-    clusters_data = None
-
+class DrawingClusters:
     @classmethod
-    def __make_dots_colors(self):
+    def __get_dots_colors(cls, clusters_data, max_noise_size):
         random.seed(10)
-        self.cluster_sizes = collections.Counter(self.clusters_data.cluster_numbers)
-        print(self.cluster_sizes)
-        for key, value in self.cluster_colors:
-            if value <= self.max_noise_size:
-                self.cluster_colors[key] = ''.join(['#', rand_color(), rand_color(), rand_color()])
+        cluster_sizes = collections.Counter(clusters_data.cluster_numbers)
+        cluster_colors = dict()
+        for key in cluster_sizes:
+            if cluster_sizes[key] >= max_noise_size:
+                cluster_colors[key] = ''.join(['#', rand_color(), rand_color(), rand_color()])
+            else:
+                cluster_colors[key] = 'red'
+        return [cluster_colors[x] for x in clusters_data.cluster_numbers]
 
     @classmethod
-    def drawing_plot(self, clusters_data: clustersData.ClustersData, max_noise_size=1):
-        self.clusters_data = clusters_data
-        self.max_noise_size = max_noise_size
-        self.__make_dots_colors()
-        print(self.clusters_data.cluster_numbers, self.cluster_colors)
-        color = [self.cluster_colors[x] for x in self.clusters_data.cluster_numbers]
-        plt.figure(figsize=(25, 25))
-        plt.scatter(x=self.clusters_data.x_init, y=self.clusters_data.y_init, c=color)
+    def drawing_plot(cls, clusters_data: clustersData.ClustersData, max_noise_size=1):
+        color = DrawingClusters.__get_dots_colors(clusters_data, max_noise_size)
+        plt.figure(figsize=(35, 35))
+        plt.scatter(x=clusters_data.x_init, y=clusters_data.y_init, c=color)
         plt.show()
