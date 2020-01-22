1253f0789da22d56027d559567965974bde1e357
stacy-s <stacey.stepanova@gmail.com>
2020-01-22 21:27:40 +0400

    The function for showing clusters on map was implemented

--- draw.py
+++ draw.py
@@ -2,7 +2,10 @@ import clustersData
 import collections
 import random
 import matplotlib.pyplot as plt
+import folium
 
+
+Point = collections.namedtuple('Point', ['x', 'y'])
 
 def rand_color():
     cur_color = hex(random.randint(0, 165))[2:]
@@ -28,3 +31,20 @@ class DrawingClusters:
         plt.figure(figsize=(35, 35))
         plt.scatter(x=clusters_data.x_init, y=clusters_data.y_init, c=color)
         plt.show()
+
+    @classmethod
+    def drawing_map(cls, clusters_data: clustersData.ClustersData, filename, city_lat_long : Point, max_noise_size=1):
+        color = DrawingClusters.__get_dots_colors(clusters_data, max_noise_size)
+        fmap = folium.Map([city_lat_long.x, city_lat_long.y])
+        folium.TileLayer(
+            tiles='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}{r}.png',
+            attr='My').add_to(fmap)
+        folium.TileLayer(
+            tiles='https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png',
+            attr='My').add_to(fmap)
+        folium.LayerControl().add_to(fmap)
+        for i in range(clusters_data.num_of_data):
+            folium.CircleMarker([clusters_data.x_init[i], clusters_data.y_init[i]],
+                                radius=2, fill=True, color=color[i],
+                                popup=str(clusters_data.cluster_numbers[i])).add_to(fmap)
+        fmap.save(filename + '.html')
