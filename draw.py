f245d02c734eabc7e7ec393f49dea30c4e9b178f
stacy-s <stacey.stepanova@gmail.com>
2020-06-01 20:20:56 +0400

    draw.Draw.draw_points take color_continuous_scale

--- draw.py
+++ draw.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 class Draw:
     @classmethod
-    def draw_points(cls, filename, lat, lon, hover_name, hover_data, color):
+    def draw_points(cls, filename, lat, lon, hover_name, hover_data, color, color_continuous_scale):
         df = pd.read_csv(filename)
         fig = px.scatter_mapbox(df, lat=lat, lon=lon, hover_name=df[color], hover_data=hover_data,
                                 color=df[color], zoom=5, height=1000,
-                                color_continuous_scale=px.colors.sequential.Hot,
+                                color_continuous_scale=color_continuous_scale,
                                 )
         fig.update_layout(mapbox_style="open-street-map")
         fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
