52f3d79ad5fc1116e6dd1e6336719752f2ce36bd
stacy-s <stacey.stepanova@gmail.com>
2020-06-02 16:24:31 +0400

    BugFix: px.scatter_mapbox has taken the wrong argument in function draw.Draw.draw_points.'

--- draw.py
+++ draw.py
@@ -6,8 +6,13 @@ class Draw:
     @classmethod
     def draw_points(cls, filename, lat, lon, hover_name, hover_data, color, color_continuous_scale):
         df = pd.read_csv(filename)
-        fig = px.scatter_mapbox(df, lat=lat, lon=lon, hover_name=color, hover_data=hover_data,
-                                color=color, zoom=5, height=1000,
+        fig = px.scatter_mapbox(df,
+                                lat=lat, lon=lon,
+                                hover_name=hover_name,
+                                hover_data=hover_data,
+                                color=color,
+                                zoom=5,
+                                height=1000,
                                 color_continuous_scale=color_continuous_scale,
                                 )
         fig.update_layout(mapbox_style="open-street-map")
